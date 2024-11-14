b6a008bc6c210037bbbc8e7f6e35356c7233f4a0
Dakodah Spurlin <dcspurlin@gmail.com>
2024-11-14 14:35:43 -0600

    Started getting the GET routes communicating.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,9 +1,22 @@
 console.log( 'js' );
+getKoalas();
 
 function getKoalas(){
   console.log( 'in getKoalas' );
   // axios call to server to get koalas
-  
+  axios({
+    method: 'GET',
+    url: '/koalas'
+  })
+  .then((response) => {
+    console.log('GET koalas!!!')
+    // ! render function goes here
+    renderKoalas(response.data)
+  })
+  .catch((error) => {
+    console.log('ERROR in GET koalas', error)
+    app.sendStatus(500)
+  })
 } // end getKoalas
 
 function saveKoala(){
@@ -12,4 +25,7 @@ function saveKoala(){
  
 }
 
-getKoalas();
+function renderKoalas(listOfKoalas) {
+  console.log('in renderKoalas')
+}
+
--- server/server.js
+++ server/server.js
@@ -6,8 +6,31 @@ const koalaRouter = require('./routes/koalas.router');
 app.use(express.json());
 app.use(express.static('server/public'));
 
+koalaList = [
+  {
+    name: 'Scotty', 
+    gender: 'M', 
+    age: 4,
+    readyToTransfer: true, 
+    notes: 'Born in Guatemala'
+  },
+  {
+    name: 'Jean', 
+    gender: 'F', 
+    age: 5,
+    readyToTransfer: true, 
+    notes: 'Allergic to lots of lava'
+  }
+
+]
+
 // ROUTES
 app.use('/koalas', koalaRouter);
+
+app.get('/koalas', (req, res) => {
+  console.log(`In /koalas GET`);
+  res.send(koalaList);
+});
 
 // Start listening for requests on a specific port
 app.listen(PORT, () => {
