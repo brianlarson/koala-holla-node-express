c641be9d8916d7cc65891898c670c1336091105c
Brian Larson <brian@brianlarson.com>
2024-11-14 13:28:18 -0600

    Working on GET requestin client

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = 5001;
-const koalaRouter = require('./routes/koala.router');
+const koalaRouter = require('./routes/koalas.router');
 
 app.use(express.json());
 app.use(express.static('server/public'));
