5e6364c65b4492c7a139c6af7282758b56e6d4f6
Dakodah Spurlin <dcspurlin@gmail.com>
2024-11-14 15:12:57 -0600

    Group Project.
    Co-authored-by: Jake Berg <jake-bergg@users.noreply.github.com>

---
+++ server/routes/koalas.router.js
@@ -0,0 +1,67 @@
+const express = require('express')
+const router = express.Router()
+const pool = require('../modules/pool')
+
+router.get ( '/', (req, res) => {
+    let queryText = `SELECT * FROM "koalas";`
+    pool.query (queryText)
+    .then(( results ) =>{
+    //Return rows of result:
+    res.send ( results.rows )
+    })
+    .catch( (error) => {
+        // Handle any errors:
+        console.log("there was an error in recieving koalas: ", error)
+        res.sendStatus( 400 )
+    })
+})
+
+router.post( '/', ( req, res )=>{
+    console.log( 'in /koalas POST:', req.body );
+        // assemble query
+        const queryText = `INSERT into "koalas" ( name, birthdate ) VALUES ( $1, $2 );`;
+        const values = [ req.body.name, req.body.birthdate ];
+        // run pool.query
+        pool.query( queryText, values ).then( ( results )=>{
+            // return results.rows
+            res.sendStatus( 201 ); // "CREATED"
+        }).catch( ( err )=>{
+            // handle any errors
+            console.log( err );
+            res.sendStatus( 400 );
+        })
+})
+
+router.delete( '/', ( req, res )=>{
+    console.log( 'in /koalas DELETE:', req.body );
+        // assemble query
+        const queryText = `DELETE FROM koalas WHERE id=$1;`;
+        const values = [ req.body.id ];
+        // run pool.query
+        pool.query( queryText, values ).then( ( results )=>{
+            res.sendStatus( 200 ); // "OK"
+        }).catch( ( err )=>{
+            // handle any errors
+            console.log( err );
+            res.sendStatus( 400 );
+        })
+})
+
+router.put( '/', ( req, res )=>{
+    console.log( '/koalas PUT:', req.body );
+    const queryText = `UPDATE koalas SET favorite=$1 WHERE id=$2;`;
+    const values = [ req.body.newFavorite, req.body.id ];
+    // run pool.query
+    pool.query( queryText, values ).then( ( results )=>{
+        res.sendStatus( 200 ); // "OK"
+    }).catch( ( err )=>{
+        // handle any errors
+        console.log( err );
+        res.sendStatus( 400 );
+    })
+})
+
+
+
+
+module.exports = router;
