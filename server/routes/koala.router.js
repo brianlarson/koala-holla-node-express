3d81916b0ed9d65ed78dc30f5b42fb142fdc49e0
Brian Larson <brian@brianlarson.com>
2024-11-14 13:24:50 -0600

    Set up db connection with pg and start GET route

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -2,10 +2,12 @@ const express = require('express');
 const koalaRouter = express.Router();
 
 // DB CONNECTION
-
+const pool = require('../modules/pool');
 
 // GET
-
+koalaRouter.get('/', (req, res) => {
+  console.log("/koala GET request received…");
+});
 
 // POST
 
