0d97d63793878e5ef795d9d102b26f3f5c1a7274
Dakodah Spurlin <dcspurlin@gmail.com>
2024-11-14 15:55:31 -0600

    Finish POST request and start delete btn

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -28,7 +28,7 @@ function saveKoala(){
 function addKoala(){
   console.log('lets add a koala')
 
-
+// ! create newKoala object to hold inputs
   const newKoala = {
     name: document.getElementById('nameIn').value, 
     gender: document.getElementById('colorIn').value, 
@@ -38,6 +38,7 @@ function addKoala(){
   }
   console.log('Lets welcome the new guy: ', newKoala)
 
+// ! axios POST request to send data to server
   axios({
     method: 'POST',
     url: '/koalas',
@@ -45,6 +46,7 @@ function addKoala(){
   })
   .then((response) => {
     console.log('POST koalas!!')
+    // ! call getKoalas() function to send new koala inputs to server
     getKoalas()
   })
   .catch((error) => {
@@ -56,7 +58,10 @@ function addKoala(){
 function renderKoalas(listOfKoalas) {
   console.log('in renderKoalas')
   const koalasTableBody = document.getElementById('viewKoalas')
+  // ! clear table before sending new inputs
   koalasTableBody.innerHTML = ''
+
+  // ! new inputs get sent into HTML
   for(let koala of listOfKoalas){
     koalasTableBody.innerHTML += `
       <tr>
@@ -65,6 +70,7 @@ function renderKoalas(listOfKoalas) {
         <td>${koala.gender}</td>
         <td>${koala.ready_to_transfer}</td>
         <td>${koala.notes}</td>  
+        <button onClick="deleteKoala(event)">Delete</button>
       </tr>  
     `
   }
